dd0e954263b9c64a93b184dfbe94ed22eab41299
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-05-21 18:47:35 -0400

    began work on toString method

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,54 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value) {
+    // Start out with a value
+    this.value = value;
+    // That value does not know about the next value in the chain
+    this.next = null;
+  }
 }
 
-module.exports = LinkedList;
+class LinkedList {
+  constructor() {
+    // When Instantiating a linked list nothing is passed into that list, and it is completely empy
+    this.head = null;
+  }
+  // adds a node to the FRONT of a linked list
+  insert(value) {
+    let node = new Node(value);
+    node.next = this.head;
+    // If the head is null make the new node the head
+    this.head = node;
+  }
+  // adds a node to the END of a linked list
+  append(value) {
+    let node = new Node(value);
+    // if the head is null, we need to assign this new node to the head as well
+    if(!this.head) {
+      // in other words, if the list was empty do this and done
+      this.head = node;
+      return;
+    }
+    let current = this.head;
+    while(current.next){
+      current = current.next;
+    }
+    current.next = node;
+  }
+  // iteratively travers linked list and do a thing
+  traversal() {
+    let current = this.head;
+    while(current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+// IN PROGRESS toString method
+  toString(value) {
+    let result = this.head
+    next = result
+  }
+}
+
+module.exports = LinkedList
